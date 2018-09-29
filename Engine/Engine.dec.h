7bd0ba8cc0ad427c339f1c4acdce81179aca034d
pragma-once <hashpragmaonce@gmail.com>
2018-09-29 15:20:37 +0330

    Use 'typename' instead of 'class' in template parameters

--- Engine/Engine.dec.h
+++ Engine/Engine.dec.h
@@ -6,13 +6,13 @@ namespace Engine
     {
         namespace Collections
         {
-            template <class ItemsType, bool UseMutex = true> class ResizableArray;
-            template <class ItemsType, bool UseMutex = true> class List;
-            template <class ItemsType, bool UseMutex = true> class ReadOnlyList;
-            template <class ItemsType, bool UseMutex = true> class Stack;
-            template <class ItemsType, bool UseMutex = true> class Queue;
-            template <class ItemsType, class PriorityType = int, bool UseMutex = true> class PriorityQueue;
-            template <class ValuesType, bool UseMutex = true> class Dictionary;
+            template <typename ItemsType, bool UseMutex = true> class ResizableArray;
+            template <typename ItemsType, bool UseMutex = true> class List;
+            template <typename ItemsType, bool UseMutex = true> class ReadOnlyList;
+            template <typename ItemsType, bool UseMutex = true> class Stack;
+            template <typename ItemsType, bool UseMutex = true> class Queue;
+            template <typename ItemsType, class PriorityType = int, bool UseMutex = true> class PriorityQueue;
+            template <typename ValuesType, bool UseMutex = true> class Dictionary;
         }
     }
 }
