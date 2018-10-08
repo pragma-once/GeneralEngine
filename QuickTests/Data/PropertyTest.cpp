25b4eb8c127809badf67974c42a3575abb852beb
pragma-once <hashpragmaonce@gmail.com>
2018-10-08 15:08:59 +0330

    Implement Property quick test

---
+++ QuickTests/Data/PropertyTest.cpp
@@ -0,0 +1,46 @@
+#include "../../Engine/Engine.h"
+#include <iostream>
+#include <string>
+
+#define print(content) (std::cout << content << '\n')
+
+#define TestType std::string
+
+class TestClass
+{
+public:
+    std::string Name = "Unknown";
+    Engine::Data::Property<TestType> A;
+    Engine::Data::Property<TestType, false, true> B;
+    Engine::Data::Property<TestType, true, false> C;
+    // Engine::Data::Property<TestType, false, false> D; //-> error
+
+    TestClass(TestType A, TestType B, TestType C)
+    {
+        a = A;
+        b = B;
+        c = C;
+
+        this->A.SetSetter([this](TestType Value) { a = Value; print(Name << ".A set to " << Value); });
+        this->A.SetGetter([this]() { print("Getting " << Name << ".A: " << a); return a; });
+        this->B.SetGetter([this]() { print("Getting " << Name << ".B: " << b); return b; });
+        this->C.SetSetter([this](TestType Value) { c = Value; print(Name << ".C set to " << Value); });
+    }
+private:
+    TestType a;
+    TestType b;
+    TestType c;
+};
+
+int main()
+{
+    TestClass test("AA", "BB", "CC");
+    test.A = test.B;
+    //test.A = test.C; //->error
+    //test.B = test.A; //->error
+    //test.B = test.C; //->error
+    test.C = test.A;
+    test.C = test.B;
+
+    return 0;
+}
