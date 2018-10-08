5bf193c926e4fab2e065848624a2a0f48436bbf5
pragma-once <hashpragmaonce@gmail.com>
2018-10-08 14:10:16 +0330

    Complete Property class

--- Engine/Data/Property.h
+++ Engine/Data/Property.h
@@ -1,4 +1,10 @@
-#pragma once
+#ifndef ENGINE_PROPERTY_INCLUDED
+
+#ifndef ENGINE_PROPERTY_NO_SETTER
+    #ifdef ENGINE_PROPERTY_NO_GETTER
+        #define ENGINE_PROPERTY_INCLUDED
+    #endif
+#endif
 
 #include "../Engine.dec.h"
 
@@ -8,17 +14,21 @@ class Property<Type, true, true>
 public:
     Property(const Property&) = delete;
 
-    Property()
-    {
-        Setter = nullptr;
-        Getter = nullptr;
-    }
+    Property() {}
 
     Property(std::function<void(Type)> Setter, std::function<Type()> Getter)
     {
+
+#ifndef ENGINE_PROPERTY_NO_SETTER
         this->Setter = Setter;
+#endif
+#ifndef ENGINE_PROPERTY_NO_GETTER
         this->Getter = Getter;
+#endif
+
     }
+
+#ifndef ENGINE_PROPERTY_NO_SETTER
 
     void SetSetter(std::function<void(Type)> Setter)
     {
@@ -27,12 +37,20 @@ public:
         this->Setter = Setter;
     }
 
+#endif
+
+#ifndef ENGINE_PROPERTY_NO_GETTER
+
     void SetGetter(std::function<void(Type)> Getter)
     {
         if (this->Getter != nullptr)
             throw std::logic_error("Cannot set the Getter more than once.");
         this->Getter = Getter;
     }
+
+#endif
+
+#ifndef defined(ENGINE_PROPERTY_NO_SETTER) && defined(ENGINE_PROPERTY_NO_GETTER)
 
     int operator=(const Property& Operand)
     {
@@ -41,17 +59,53 @@ public:
         return Value;
     }
 
+#else
+
+    int operator=(const Property& Operand) = delete;
+
+#endif
+
+#ifndef ENGINE_PROPERTY_NO_SETTER
+
     int operator=(const Type& Value)
     {
         Setter(Value);
         return Value;
     }
 
+#endif
+
+#ifndef ENGINE_PROPERTY_NO_GETTER
+
     operator Type()
     {
         return Getter();
     }
+
+#endif
+
 private:
+
+#ifndef ENGINE_PROPERTY_NO_SETTER
+
     std::function<void(Type)> Setter = nullptr;
+
+#endif
+
+#ifndef ENGINE_PROPERTY_NO_GETTER
+
     std::function<Type()> Getter = nullptr;
+
+#endif
+
 };
+
+#define ENGINE_PROPERTY_NO_SETTER
+#include "Property.h"
+#undef ENGINE_PROPERTY_NO_SETTER
+
+#define ENGINE_PROPERTY_NO_GETTER
+#include "Property.h"
+#undef ENGINE_PROPERTY_NO_GETTER
+
+#endif // Include Guard
