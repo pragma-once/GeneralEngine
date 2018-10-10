44c3feeddc0ee2c2747f486314791865217c08eb
pragma-once <hashpragmaonce@gmail.com>
2018-10-11 00:00:07 +0330

    Complete List quick test

--- QuickTests/Data/Collections/ListTest.cpp
+++ QuickTests/Data/Collections/ListTest.cpp
@@ -23,66 +23,95 @@ int main()
         print("e Item        => Exists(Item)");
         print("F             => ForEach([](Item) { print(Item); }");
         print("");
-        print("A Item Times  => for Times: Add(Item)");
-        print("d             => delete list; list = new List");
+        print("A Item Times      => for Times: Add(Item)");
+        print("d                 => delete list; list = new List()");
+        print("n InitialCapacity => delete list; list = new List(InitialCapacity)");
+        print("");
+        print("E Space           => Expand(Space)");
+        print("S AdditionalSpace => Shrink(AdditionalSpace)");
+        print("L                 => GetCapacity()");
         print("");
         char func;
         int arg_int;
         ITEMS_TYPE arg_str;
         input(func);
-        switch (func)
+
+        try
         {
-        case 'a':
-            input(arg_str);
-            print(list->Add(arg_str));
-            break;
-        case 'p':
-            input(arg_str);
-            input(arg_int);
-            print(list->Add(arg_str, arg_int));
-            break;
-        case 'r':
-            input(arg_int);
-            print(list->Remove(arg_int));
-            break;
-        case 's':
-            input(arg_int);
-            input(arg_str);
-            print(list->SetItem(arg_int, arg_str));
-            break;
-        case 'c':
-            print(list->Clear());
-            break;
-        case 'g':
-            input(arg_int);
-            print(list->GetItem(arg_int));
-            break;
-        case 'C':
-            print(list->GetCount());
-            break;
-        case 'f':
-            input(arg_str);
-            input(arg_int);
-            print(list->Find(arg_str, arg_int));
-            break;
-        case 'e':
-            input(arg_str);
-            print(list->Exists(arg_str));
-            break;
-        case 'F':
-            list->ForEach([](ITEMS_TYPE Item) { print(Item); });
-            break;
-        case 'A':
-            input(arg_str);
-            input(arg_int);
-            for (int i = 0; i < arg_int; i++) list->Add(arg_str);
-            break;
-        case 'd':
-            delete list;
-            list = new Engine::Data::Collections::List<ITEMS_TYPE>();
-            break;
-        default:
-            break;
+            auto old_list = list;
+            switch (func)
+            {
+            case 'a':
+                input(arg_str);
+                print(list->Add(arg_str));
+                break;
+            case 'p':
+                input(arg_str);
+                input(arg_int);
+                print(list->Add(arg_str, arg_int));
+                break;
+            case 'r':
+                input(arg_int);
+                print(list->Remove(arg_int));
+                break;
+            case 's':
+                input(arg_int);
+                input(arg_str);
+                print(list->SetItem(arg_int, arg_str));
+                break;
+            case 'c':
+                print(list->Clear());
+                break;
+            case 'g':
+                input(arg_int);
+                print(list->GetItem(arg_int));
+                break;
+            case 'C':
+                print(list->GetCount());
+                break;
+            case 'f':
+                input(arg_str);
+                input(arg_int);
+                print(list->Find(arg_str, arg_int));
+                break;
+            case 'e':
+                input(arg_str);
+                print(list->Exists(arg_str));
+                break;
+            case 'F':
+                list->ForEach([](ITEMS_TYPE Item) { print(Item); });
+                break;
+            case 'A':
+                input(arg_str);
+                input(arg_int);
+                for (int i = 0; i < arg_int; i++) list->Add(arg_str);
+                break;
+            case 'd':
+                delete list;
+                list = new Engine::Data::Collections::List<ITEMS_TYPE>();
+                break;
+            case 'n':
+                input(arg_int);
+                list = new Engine::Data::Collections::List<ITEMS_TYPE>(arg_int);
+                delete old_list;
+                break;
+            case 'E':
+                input(arg_int);
+                list->Expand(arg_int);
+                break;
+            case 'S':
+                input(arg_int);
+                list->Shrink(arg_int);
+                break;
+            case 'L':
+                print(list->GetCapacity());
+            default:
+                break;
+            }
+        }
+        catch (std::exception& e)
+        {
+            print("Exception: " << e.what());
         }
     }
 
