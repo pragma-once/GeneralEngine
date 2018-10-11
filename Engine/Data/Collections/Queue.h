cb561fe66e98a4ee2f0b1e22122a93f5483dd5f1
pragma-once <hashpragmaonce@gmail.com>
2018-10-11 14:26:57 +0330

    Rename 'stack' to 'queue'

--- Engine/Data/Collections/Queue.h
+++ Engine/Data/Collections/Queue.h
@@ -181,7 +181,7 @@ namespace Engine
                 ENGINE_COLLECTION_READ_ACCESS;
 
                 if (Count <= 0)
-                    throw std::logic_error("Cannot get the top element of an empty stack.");
+                    throw std::logic_error("Cannot get the top element of an empty queue.");
 
                 return Items->GetItem(First);
             }
