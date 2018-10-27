37764a7a24b4e9c1bebe4df0f572b47cb0512f0d
pragma-once <hashpragmaonce@gmail.com>
2018-10-27 22:11:22 +0330

    Include all Collections in Engine.h

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -3,3 +3,7 @@
 
 #include "Data/Collections/ResizableArray.h"
 #include "Data/Collections/List.h"
+#include "Data/Collections/Stack.h"
+#include "Data/Collections/Queue.h"
+#include "Data/Collections/PriorityQueue.h"
+#include "Data/Collections/Dictionary.h"
