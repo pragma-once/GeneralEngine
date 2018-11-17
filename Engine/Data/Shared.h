9bbd05dafb016a4dab10482b0f60bb78ed6a078f
pragma-once <hashpragmaonce@gmail.com>
2018-11-17 20:19:40 +0330

    Implement Shared class

---
+++ Engine/Data/Shared.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "../Engine.dec.h"
+
+namespace Engine
+{
+    namespace Data
+    {
+        template <typename Type>
+        class Shared
+        {
+        public:
+            Shared(const Shared&) = delete;
+            Shared(const Type&);
+            Type operator=(const Shared&);
+            Type operator=(const Type&);
+            operator Type();
+
+            Type Set(const Type&);
+            Type Get();
+        private:
+            Type Value;
+            std::shared_mutex Mutex;
+        };
+    }
+}
+
+// DEFINITION ----------------------------------------------------------------
+
+namespace Engine
+{
+    namespace Data
+    {
+        template <typename Type>
+        Shared<Type>::Shared(const Type& Value)
+        {
+            std::lock_guard<std::shared_mutex> Guard(Mutex);
+            this->Value = Value;
+        }
+
+        template <typename Type>
+        Type Shared<Type>::operator=(const Shared<Type>& Operand)
+        {
+            std::shared_lock<std::shared_mutex> guard(Operand.Mutex);
+            Type Value = Operand.Value;
+            guard.unlock();
+            std::lock_guard<std::shared_mutex> guard(Mutex);
+            this->Value = Value;
+            return Value;
+        }
+
+        template <typename Type>
+        Type Shared<Type>::operator=(const Type& Value)
+        {
+            std::lock_guard<std::shared_mutex> guard(Mutex);
+            this->Value = Value;
+            return Value;
+        }
+
+        template <typename Type>
+        Shared<Type>::operator Type()
+        {
+            std::shared_lock<std::shared_mutex> guard(Mutex);
+            return Value;
+        }
+
+        template <typename Type>
+        Type Shared<Type>::Set(const Type& Value)
+        {
+            std::lock_guard<std::shared_mutex> guard(Mutex);
+            this->Value = Value;
+            return Value;
+        }
+
+        template <typename Type>
+        Type Shared<Type>::Get()
+        {
+            std::shared_lock<std::shared_mutex> guard(Mutex);
+            return Value;
+        }
+    }
+}
