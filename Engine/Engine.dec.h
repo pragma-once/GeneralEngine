732c0f1da378a5c128eca1e00c10581441dc2f23
Majidzadeh <hashpragmaonce@gmail.com>
2019-02-03 00:02:39 +0330

    Document Engine.dec.h

--- Engine/Engine.dec.h
+++ Engine/Engine.dec.h
@@ -11,13 +11,21 @@ namespace Engine
 {
     namespace Data
     {
+        /// @brief Shared mutex that handles recursive locking in a thread.
+        ///
+        /// Try to use GetLock or GetSharedLock and avoid using Locking/Unlocking functions.
         class HandledMutex;
+        /// @brief Object that uses a setter and/or a getter to be set or get.
         template <typename Type, bool HasSetter = true, bool HasGetter = true> class Property;
+        /// @brief Shared object with automatic mutex locking on set/get
         template <typename Type> class Shared;
 
         namespace Collections
         {
             template <typename ItemsType, bool UseMutex = true> class ResizableArray;
+            /// @brief Array list or array list interface.
+            ///
+            /// List interfaces define custom Add, Remove, SetItem and Clear functions for a list.
             template <typename ItemsType, bool UseMutex = true> class List;
             template <typename ItemsType, bool UseMutex = true> class Stack;
             template <typename ItemsType, bool UseMutex = true> class Queue;
@@ -28,7 +36,11 @@ namespace Engine
 
     namespace Core
     {
+        /// @brief Manages and runs Module objects.
         class Container;
+        /// @brief Abstract class to implement the application's modules.
+        ///
+        /// Add them to a Container to run.
         class Module;
     }
 }
